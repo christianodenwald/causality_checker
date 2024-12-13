21d84e9fc1dee78a5ba82dce2c1de61552b7182b
Christian <odenwaldchristian@gmail.com>
2024-12-13 17:44:30 +0000

    reformat

--- HP2015.py
+++ HP2015.py
@@ -1,40 +1,48 @@
 import copy, itertools
-### VIGNETTE 1
-
-ff_disj = {'variables':['ML', 'L', 'FF'],
-              'initial_values':[1, 1, 1],
-              'current_values':[0, 1, 1],
-              'value_ranges': [{0,1}, {0,1}, {0,1},],
-              'structural_equations':[None, None, None]}
-# ff_disj['current_values'] = ff_disj['initial_values']
-ff_disj['structural_equations'][2] = lambda: ff_disj['current_values'][0] or ff_disj['current_values'][1]
-
-### VIGNETTE 2
-
-ff_conj = copy.deepcopy(ff_disj)
-ff_conj['structural_equations'][2] = lambda: ff_conj['current_values'][0] and ff_conj['current_values'][1]
 
 
-### VIGNETTE 3
+def generate_vignettes():
+    ### VIGNETTE 1
 
-bottle_shatters = {'variables':['ST', 'BT', 'SH', 'BH', 'BS'],
-              'initial_values':[1, 1, 1, 0, 1],
-              'current_values':[None]*5,
-              'value_ranges': [{0,1}]*5,
-              'structural_equations':[None]*5}
+    ff_disj = {'variables': ['ML', 'L', 'FF'],
+               'initial_values': [1, 1, 1],
+               'current_values': [0, 1, 1],
+               'value_ranges': [{0, 1}, {0, 1}, {0, 1}, ],
+               'structural_equations': [None, None, None]}
+    # ff_disj['current_values'] = ff_disj['initial_values']
+    ff_disj['structural_equations'][2] = lambda: ff_disj['current_values'][0] or ff_disj['current_values'][1]
 
-bottle_shatters['structural_equations'][2] = lambda: bottle_shatters['current_values'][0]
-bottle_shatters['structural_equations'][3] = lambda: bottle_shatters['current_values'][1] or not bottle_shatters['current_values'][2]
-bottle_shatters['structural_equations'][4] = lambda: bottle_shatters['current_values'][2] or bottle_shatters['current_values'][3]
+    ### VIGNETTE 2
+
+    ff_conj = copy.deepcopy(ff_disj)
+    ff_conj['structural_equations'][2] = lambda: ff_conj['current_values'][0] and ff_conj['current_values'][1]
+
+    ### VIGNETTE 3
+
+    bottle_shatters = {'variables': ['ST', 'BT', 'SH', 'BH', 'BS'],
+                       'initial_values': [1, 1, 1, 0, 1],
+                       'current_values': [None] * 5,
+                       'value_ranges': [{0, 1}] * 5,
+                       'structural_equations': [None] * 5}
+
+    bottle_shatters['structural_equations'][2] = lambda: bottle_shatters['current_values'][0]
+    bottle_shatters['structural_equations'][3] = lambda: bottle_shatters['current_values'][1] or not \
+        bottle_shatters['current_values'][2]
+    bottle_shatters['structural_equations'][4] = lambda: bottle_shatters['current_values'][2] or \
+                                                         bottle_shatters['current_values'][3]
+
+    return [ff_disj, ff_conj, bottle_shatters]
+
 
 ###################################
 
 
 def powerset(iterable):
     s = list(iterable)
-    return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1)))
-def check_causality_H2015(vignette, cause_variable, cause_value, effect_variable, effect_value):
+    return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s) + 1)))
 
+
+def check_causality_H2015(vignette, cause_variable, cause_value, effect_variable, effect_value):
     # only works for single conjuncts for now, both for cause and effect
 
     ### preparation
@@ -42,7 +50,6 @@ def check_causality_H2015(vignette, cause_variable, cause_value, effect_variable
     exo_variable_index = [index for index, item in enumerate(vignette['structural_equations']) if item == None]
     cause_index = vignette['variables'].index(cause_variable)
     effect_index = vignette['variables'].index(effect_variable)
-
 
     ### AC1 is implied
 
@@ -71,14 +78,12 @@ def check_causality_H2015(vignette, cause_variable, cause_value, effect_variable
     return False
 
 
-
-
-
 if __name__ == '__main__':
-    check_causality_H2015(ff_conj, 'ML', 1, 'FF', 1) # True
-    check_causality_H2015(ff_disj, 'ML', 1, 'FF', 1) # False
-    check_causality_H2015(bottle_shatters, 'ST', 1, 'BS', 1) # True
-    check_causality_H2015(bottle_shatters, 'BT', 1, 'BS', 1) # False
+    ff_disj, ff_conj, bottle_shatters = generate_vignettes()
 
+    check_causality_H2015(ff_conj, 'ML', 1, 'FF', 1)  # True
+    check_causality_H2015(ff_disj, 'ML', 1, 'FF', 1)  # False
+    check_causality_H2015(bottle_shatters, 'ST', 1, 'BS', 1)  # True
+    check_causality_H2015(bottle_shatters, 'BT', 1, 'BS', 1)  # False
 
 print()

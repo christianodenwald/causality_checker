b437226935107cd52e912901568d58f675daa924
Christian Odenwald <odenwaldchristian@gmail.com>
2025-01-08 14:53:14 +0000

    revert changes; powerset might be enough

--- HP2015.py
+++ HP2015.py
@@ -1,26 +1,28 @@
 import copy, itertools
-from HP2005 import all_splits_with_mandatory_element
+# from HP2005 import all_splits_with_mandatory_element
 
 
 def generate_vignettes():
     ### VIGNETTE 1
 
-    ff_disj = {'variables': ['ML', 'L', 'FF'],
+    ff_disj = { 'name': 'FF Disjunctive',
+        'variables': ['ML', 'L', 'FF'],
                'initial_values': [1, 1, 1],
-               'current_values': [0, 1, 1],
+               'current_values': [None]*3,
                'value_ranges': [{0, 1}, {0, 1}, {0, 1}, ],
                'structural_equations': [None, None, None]}
-    # ff_disj['current_values'] = ff_disj['initial_values']
     ff_disj['structural_equations'][2] = lambda: ff_disj['current_values'][0] or ff_disj['current_values'][1]
 
     ### VIGNETTE 2
 
     ff_conj = copy.deepcopy(ff_disj)
+    ff_conj['name'] = 'FF Conjunctive'
     ff_conj['structural_equations'][2] = lambda: ff_conj['current_values'][0] and ff_conj['current_values'][1]
 
     ### VIGNETTE 3
 
     bottle_shatters = {'variables': ['ST', 'BT', 'SH', 'BH', 'BS'],
+                       'name': 'Bottle Shatters',
                        'initial_values': [1, 1, 1, 0, 1],
                        'current_values': [None] * 5,
                        'value_ranges': [{0, 1}] * 5,
@@ -38,13 +40,33 @@ def generate_vignettes():
 ###################################
 
 
+def all_splits_with_mandatory_element(lst, mandatory_element):
+    if mandatory_element not in lst:
+        raise ValueError("The mandatory element must be in the list.")
+
+    lst_without_mandatory = [x for x in lst if x != mandatory_element]
+    all_splits = []
+    n = len(lst_without_mandatory)
+
+    for i in range(n + 1):
+        for combo in itertools.combinations(lst_without_mandatory, i):
+            list1 = list(combo) + [mandatory_element]
+            list2 = [x for x in lst if x not in list1]
+            all_splits.append((list1, list2))
+
+    return all_splits
+
+
 def powerset(iterable):
     s = list(iterable)
     return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s) + 1)))
 
 
 def check_causality(theory, vignette, cause_variable, cause_value, effect_variable, effect_value):
-    # only works for single conjuncts for now, both for cause and effect
+    # only works for atomic  for now, both for cause and effect
+    # therefore, AC3 is trivially satisfied
+
+    print(vignette['name'] + ': ')
 
     ### preparation
     endo_variable_index = [index for index, item in enumerate(vignette['structural_equations']) if item is not None]
@@ -55,18 +77,21 @@ def check_causality(theory, vignette, cause_variable, cause_value, effect_variab
     ### AC1 is implied
 
     if theory == 'HP2015':
+        print(f'According to {theory}, ', end='')
         ### AC2am
+        # set x_prime != x
+        # for now only check for one alternative value of x; should be for all but doesn't matter in binary setting
         x_prime = None
         for x in vignette['value_ranges'][cause_index]:
             if x != effect_value:
                 x_prime = x
 
-        # TODO powerset is not enough
-        for subset_w, subset_z in enumerate(all_splits_with_mandatory_element(range(len(vignette['variables'])))):  # check different settings w'
-            for i in exo_variable_index:    # set exo variables
+        for subset_w in powerset(endo_variable_index):
+            for i in exo_variable_index:
                 vignette['current_values'][i] = vignette['initial_values'][i]
-            for i in endo_variable_index:   # clear endo variables; not necessary
+            for i in endo_variable_index:
                 vignette['current_values'][i] = None
+            # print(subset_w)
             # set X=x' and W=w'
             for i in subset_w:
                 vignette['current_values'][i] = vignette['initial_values'][i]
@@ -76,10 +101,32 @@ def check_causality(theory, vignette, cause_variable, cause_value, effect_variab
                 if i not in subset_w:
                     vignette['current_values'][i] = int(vignette['structural_equations'][i]())
             if vignette['current_values'][effect_index] != effect_value:
-                return True
-        return False
+                # return True
+                print(f'{cause_variable}={cause_value} IS an actual cause of {effect_variable}={effect_value}\n')
+                break
+        # return False
+        else:
+            print(f'{cause_variable}={cause_value} is NOT an actual cause of {effect_variable}={effect_value}\n')
 
-    elif theory == 'HP2015':
+        # # TODO powerset is not enough
+        # for subset_w, subset_z in all_splits_with_mandatory_element(range(len(vignette['variables'])), cause_index):  # check different settings w'
+        #     for i in exo_variable_index:    # set exo variables
+        #         vignette['current_values'][i] = vignette['initial_values'][i]
+        #     for i in endo_variable_index:   # clear endo variables; not necessary
+        #         vignette['current_values'][i] = None
+        #     # set X=x' and W=w'
+        #     for i in subset_w:
+        #         vignette['current_values'][i] = vignette['initial_values'][i]
+        #     vignette['current_values'][cause_index] = x_prime
+        #     # propagate with newly set values
+        #     for i in endo_variable_index:
+        #         if i not in subset_w:
+        #             vignette['current_values'][i] = int(vignette['structural_equations'][i]())
+        #     if vignette['current_values'][effect_index] != effect_value:
+        #         return True
+        # return False
+
+    elif theory == 'HP2005':
         pass
 
     else:
@@ -87,18 +134,17 @@ def check_causality(theory, vignette, cause_variable, cause_value, effect_variab
 
 
 
-
-
-
+def evaluate_on_all_vignettes(theory):
+    check_causality(theory, ff_conj, 'ML', 1, 'FF', 1)  # True
+    check_causality(theory, ff_disj, 'ML', 1, 'FF', 1)  # False
+    check_causality(theory, bottle_shatters, 'ST', 1, 'BS', 1)  # True
+    check_causality(theory, bottle_shatters, 'BT', 1, 'BS', 1)  # False
 
 
 if __name__ == '__main__':
     ff_disj, ff_conj, bottle_shatters = generate_vignettes()
 
-    check_causality('HP2005', ff_conj, 'ML', 1, 'FF', 1)  # True
-    check_causality('HP2005', ff_disj, 'ML', 1, 'FF', 1)  # False
-    check_causality('HP2005', bottle_shatters, 'ST', 1, 'BS', 1)  # True
-    check_causality('HP2005', bottle_shatters, 'BT', 1, 'BS', 1)  # False
+    evaluate_on_all_vignettes('HP2015')
 
 
 print()
